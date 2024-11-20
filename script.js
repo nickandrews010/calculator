70e48c9a504dcd080f31ba38a593c51b024a433e
Nick <nickandrews010@gmail.com>
2024-11-19 17:52:16 -0800

    Create functions for basic operations (+-/*)

---
+++ script.js
@@ -0,0 +1,20 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+console.log("Added: " + add(7,7));
+console.log("Subtracted: " + subtract(7,7));
+console.log("Multiplied: " + multiply(7,7));
+console.log("Divided: " + divide(7,7));
